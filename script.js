143fcf523d7df4cb9fc4def437c05adf322fbb0c
Glitch (dandy-machine) <none>
2019-06-05 22:44:56 +0000

    Added the this test

--- script.js
+++ script.js
@@ -2,9 +2,22 @@
     to your site with Javascript */
 
 // prints "hi" in the browser's dev tools console
-console.log('hi');
 
 function add(a, b) {
   return a + b;
 }
 
+function forEach(arr, callback, thisObject) {
+  let fcnCallback = callback
+  let modified_arr = [];
+  
+  if (thisObject) {
+    fcnCallback = callback.bind(thisObject)
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    modified_arr[i] = fcnCallback(arr[i],i, arr);
+  };
+  
+  return modified_arr
+};
